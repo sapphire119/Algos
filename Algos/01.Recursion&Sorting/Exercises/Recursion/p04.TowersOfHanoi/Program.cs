ca2e9132be94280f91ab617665e60b3f61bf4641
sapphire119 <sap.phire@abv.bg>
2020-05-23 20:36:19 +0300

    Solve Tower Of Hanoi

--- Algos/01.Recursion&Sorting/Exercises/Recursion/p04.TowersOfHanoi/Program.cs
+++ Algos/01.Recursion&Sorting/Exercises/Recursion/p04.TowersOfHanoi/Program.cs
@@ -2,125 +2,147 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel;
+    using System.Linq;
+    using System.Reflection.PortableExecutable;
+    using System.Runtime.InteropServices;
     using System.Security.Cryptography;
 
     public class Program
     {
         public static void Main()
         {
-            var input = 6;
+            var input = 3;
 
-            var source = new Stack<int>();
-
-            for (int i = input; i >= 1; i--) source.Push(i);
-            //for (int i = 1; i <= input; i++) source.Push(i);
-
+            var source = new Stack<int>(Enumerable.Range(1, input).Reverse());
             var destination = new Stack<int>();
             var spare = new Stack<int>();
 
-            BuildTower(input, source, destination, spare);
+            var towerHanoi = new Dictionary<int, Stack<int>>
+            {
+                { 0, source },
+                { 1, spare },
+                { 2, destination }
+            };
+
+            var iteratations = 0;
+            PrintDisks(towerHanoi);
+            BuildTower(1, input, towerHanoi, ref iteratations);
         }
 
-        private static void BuildTower(int input, Stack<int> from, Stack<int> to, Stack<int> spare)
+        private static void BuildTower(int input, int limit, Dictionary<int, Stack<int>> towerHanoi, ref int numberOfIterations)
         {
-            if (input > 0)
-            {
-                BuildTower(input - 1, from, to, spare);
-                if (from.Contains(input))
-                {
-                    SetElementsFrom(input, from, to, spare);
-                }
-                else if (spare.Contains(input))
-                {
-                }
-                else if (to.Contains(input))
-                {
-                }
-                
-            }
-
             //n - 1 -> A ... B
-            //SolveTowerByInput(input, from, to, spare);
-
-            //switch to last element --> A ... C
-            //var elementToTransfer = from.Pop();
-            //if (elementToTransfer == input)
-            //    to.Push(elementToTransfer);
-            //else
-            //    throw new ArgumentException();
-
-            //n - 1 -> B ... C
-            //SolveTowerByInput(input - 1, from, to, spare);
-        }
-
-        private static void SetElementsFrom(int element, Stack<int> from, Stack<int> to, Stack<int> spare)
-        {
-            if (from.Count > 0)
+            //last ele -> set to position
+            if (input <= limit)
             {
-                var lastEleFrom = from.Peek();
-                if (from.Count % 2 == 1)
-                {
-                    if (to.Count > 0)
-                    {
-                        var toLastEle = to.Peek();
-                        if (element > toLastEle)
-                        {
-                            BuildTower(toLastEle, from, to, spare);
-                        }
-                    }
-
-                    to.Push(lastEleFrom);
-                }
-                else
-                {
-
-                    if (spare.Count > 0)
-                    {
-                        var spareLastEle = spare.Peek();
-                        if (element > spareLastEle)
-                        {
-                            BuildTower(spareLastEle, from, to, spare);
-                        }
-                    }
-
-                    spare.Push(lastEleFrom);
-                }
-
-                from.Pop();
+                SetTower(input, limit, towerHanoi, ref numberOfIterations);
+                BuildTower(input + 1, limit, towerHanoi, ref numberOfIterations);
+            }
+            //n - 1 -> B ... C
+            if (input < limit)
+            {
+                SetTower(input, limit, towerHanoi, ref numberOfIterations);
             }
         }
 
-        //private static void SetElementToStack(int element, Stack<int> currentTargetStack)
-        //{
-        //    //if (currentTargetStack.Count > 0)
-        //    //{
-        //    //    var toLastEle = currentTargetStack.Peek();
-        //    //    if (element > toLastEle)
-        //    //    {
-        //    //        BuildTower(toLastEle, from, to, spare);
-        //    //    }
-        //    //}
+        private static void SetTower(int input, int limit, Dictionary<int, Stack<int>> towerHanoi,
+            ref int numberOfIterations)
+        {
+            var source = towerHanoi[0];
+            var spare = towerHanoi[1];
+            var destination = towerHanoi[2];
 
-        //    //currentTargetStack.Push(lastEleFrom);
-        //}
+            if (source.Contains(input))
+            {
+                //A -> C odd
+                //A -> B even
+                SetElements(input, limit, source, destination, spare, towerHanoi, ref numberOfIterations);
+            }
+            else if (spare.Contains(input))
+            {
+                //B -> C odd
+                //B -> A even
+                SetElements(input, limit, spare, destination, source, towerHanoi, ref numberOfIterations);
+            }
+            else if (destination.Contains(input))
+            {
+                //C -> B odd
+                //C -> A even
+                SetElements(input, limit, destination, spare, source, towerHanoi, ref numberOfIterations);
+            }
+        }
 
-        //private static void SolveTowerByInput(int currentEle, Stack<int> from, Stack<int> to, Stack<int> spare)
-        //{
-        //    if (currentEle > 0)
-        //    {
-        //        SolveTowerByInput(currentEle - 1, from, to, spare);
-        //        if (from.Contains(currentEle)) 
-        //        {
-        //        }
-        //        else if (to.Contains(currentEle))
-        //        {
+        private static void SetElements(int element, int limit,
+            Stack<int> source, Stack<int> destination, Stack<int> spare, Dictionary<int, Stack<int>> towerHanoi,
+            ref int numberOfIterations)
+        {
+            if (source.Count > 0)
+            {
+                while (element > source.Peek())
+                {
+                    SetElements(source.Peek(), limit, source, destination, spare, towerHanoi, ref numberOfIterations);
+                }
 
-        //        }
-        //        else if (spare.Contains(currentEle))
-        //        {
-        //        }
-        //    }
-        //}
+                var lastEleSource = source.Peek();
+
+                if (source.Count % 2 == 1) //Check if odd
+                {
+                    while ((destination.Count > 0 && element > destination.Peek() && limit > destination.Peek()) ||
+                        lastEleSource != source.Peek())
+                    {
+                        while (destination.Count > 0 && element > destination.Peek() && limit > destination.Peek())
+                        {
+                            SetTower(destination.Peek(), lastEleSource, towerHanoi, ref numberOfIterations);
+                        }
+
+                        while (lastEleSource != source.Peek())
+                        {
+                            SetElements(source.Peek(), limit, source, destination, spare, towerHanoi, ref numberOfIterations);
+                        }
+                    }
+
+                    destination.Push(lastEleSource);
+                }
+                else //else Even
+                {
+                    while ((spare.Count > 0 && element > spare.Peek() && limit > spare.Peek()) ||
+                        lastEleSource != source.Peek())
+                    {
+                        while (spare.Count > 0 && element > spare.Peek() && limit > spare.Peek())
+                        {
+                            SetTower(spare.Peek(), lastEleSource, towerHanoi, ref numberOfIterations);
+                        }
+                        while (lastEleSource != source.Peek())
+                        {
+                            SetElements(source.Peek(), limit, source, destination, spare, towerHanoi, ref numberOfIterations);
+                        }
+                    }
+
+                    spare.Push(lastEleSource);
+                }
+
+                numberOfIterations++;
+                source.Pop();
+                PrintSteps(towerHanoi, lastEleSource, ref numberOfIterations);
+            }
+        }
+
+        private static void PrintSteps(Dictionary<int, Stack<int>> towerHanoi, int currentEle, ref int numberOfIterations)
+        {
+            Console.WriteLine($"Step #{numberOfIterations}: Moved disk {currentEle}");
+            PrintDisks(towerHanoi);
+        }
+
+        private static void PrintDisks(Dictionary<int, Stack<int>> towerHanoi)
+        {
+            var source = towerHanoi[0];
+            var spare = towerHanoi[1];
+            var destination = towerHanoi[2];
+
+            Console.WriteLine($"Source: {string.Join(", ", source.Reverse())}");
+            Console.WriteLine($"Destination: {string.Join(", ", destination.Reverse())}");
+            Console.WriteLine($"Spare: {string.Join(", ", spare.Reverse())}");
+            Console.WriteLine();
+        }
     }
 }
