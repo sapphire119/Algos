f64d659cbed3decb563b401259f940f3d89e014c
sapphire119 <sap.phire@abv.bg>
2020-05-24 17:50:35 +0300

    Solve Tower of Hanoi v2

--- Algos/01.Recursion&Sorting/Exercises/Recursion/p04.1.TowerOfHanoiV2/Program.cs
+++ Algos/01.Recursion&Sorting/Exercises/Recursion/p04.1.TowerOfHanoiV2/Program.cs
@@ -8,12 +8,33 @@
     {
         public static void Main()
         {
-            var input = 3;
-            var source = new Stack<int>(Enumerable.Range(1, input).Reverse());
+            var numberOfDisks = 4;
+
+            var source = new Stack<int>(Enumerable.Range(1, numberOfDisks).Reverse());
             var spare = new Stack<int>();
             var destination = new Stack<int>();
 
+            var firstTrigger = true;
+            var secondTrigger = true;
+            MoveDisks(numberOfDisks, source, spare, destination, firstTrigger, secondTrigger);
+            ;
+        }
 
+        private static void MoveDisks(int bottomDisk, Stack<int> source, Stack<int> spare, Stack<int> destination, bool firstTrigger, bool secondTrigger)
+        {
+            if (bottomDisk == 1)
+            {
+                destination.Push(source.Pop());
+            }
+            else
+            {
+                //n - 1 > A ... B
+                MoveDisks(bottomDisk - 1, source, destination, spare, !firstTrigger, secondTrigger);
+                //Last element -> A .. C
+                destination.Push(source.Pop());
+                //n - 1 > B ... C
+                MoveDisks(bottomDisk - 1, spare, source, destination, firstTrigger, !secondTrigger);
+            }
         }
     }
 }
