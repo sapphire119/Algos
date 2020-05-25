9d17ae479c7d2c1952ff2d6b01f0c4b03ee57f0a
sapphire119 <sap.phire@abv.bg>
2020-05-25 18:47:11 +0300

    Solve Recursion without repetition, add project for connected areas in matrix

---
+++ Algos/01.Recursion&Sorting/Exercises/Recursion/p05.CombinationsWithoutRepetition/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace p05.CombinationsWithoutRepetition
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var sequenece = int.Parse(Console.ReadLine());
+            var limit = int.Parse(Console.ReadLine());
+
+            var arr = new int[sequenece];
+            for (int i = 0, j = 1; i < arr.Length; i++, j++) arr[i] = j;
+            var vector = new int[limit];
+
+            CombineWithoutRepetition(arr, vector, 0, -1);
+        }
+
+        private static void CombineWithoutRepetition(int[] arr, int[] vector, int index, int currentIndex)
+        {
+            if (index == vector.Length)
+            {
+                Console.WriteLine(string.Join(" ", vector));
+            }
+            else
+            {
+                for (int i = currentIndex + 1; i < arr.Length; i++)
+                {
+                    vector[index] = arr[i];
+                    CombineWithoutRepetition(arr, vector, index + 1, i);
+                }
+            }
+        }
+    }
+}
---
+++ Algos/01.Recursion&Sorting/Exercises/Recursion/p06.ConnectedArea/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace p06.ConnectedArea
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
