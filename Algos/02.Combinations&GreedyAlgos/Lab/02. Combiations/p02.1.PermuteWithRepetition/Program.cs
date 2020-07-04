75901f0abe92e03d169751bca431b7c384a12fc5
sapphire119 <sap.phire@abv.bg>
2020-07-04 14:57:53 +0300

    Add solution for p02.Permutations With Repetitions

---
+++ Algos/02.Combinations&GreedyAlgos/Lab/02. Combiations/p02.1.PermuteWithRepetition/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace p02._1.PermuteWithRepetition
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            //Passing solution since sorting is not required
+            var input = Console.ReadLine().Split(' ').ToArray();
+            //Array.Sort(input);
+            PermuteWithRepetition(input, 0);
+        }
+
+        private static void PermuteWithRepetition(string[] permutation, int start)
+        {
+            if (start == permutation.Length)
+            {
+                Console.WriteLine(string.Join(" ", permutation));
+            }
+            else
+            {
+                var used = new HashSet<string>();
+                for (int i = start; i < permutation.Length; i++)
+                {
+                    if (!used.Contains(permutation[i]))
+                    {
+                        Swap(permutation, start, i);
+                        PermuteWithRepetition(permutation, start + 1);
+                        Swap(permutation, i, start);
+                        used.Add(permutation[i]);
+                    }
+                }
+            }
+        }
+        private static void Swap(string[] input, int leftIndex, int rightIndex)
+        {
+            var temp = input[leftIndex];
+            input[leftIndex] = input[rightIndex];
+            input[rightIndex] = temp;
+        }
+    }
+}
--- Algos/02.Combinations&GreedyAlgos/Lab/02. Combiations/p02.Permutations With Repetitions/Program.cs
+++ Algos/02.Combinations&GreedyAlgos/Lab/02. Combiations/p02.Permutations With Repetitions/Program.cs
@@ -2,21 +2,20 @@
 {
     using System;
     using System.Linq;
-    using System.Runtime.InteropServices.ComTypes;
 
     public class Program
     {
         public static void Main()
         {
-            var input = new string[] { "A", "B", "B" };
-            Array.Sort(input);
-
+            //var input = new string[] { "A", "B", "B" };
+            var input = Console.ReadLine().Split(' ').ToArray();
+            //Array.Sort(input);
             PermuteWithRepetition(input, 0, input.Length - 1);
         }
 
         private static void PermuteWithRepetition(string[] input, int start, int end)
         {
-
+            Print(input);
             for (int left = end - 1; left >= start; left--)
             {
                 for (int right = left + 1; right <= end; right++)
@@ -24,9 +23,21 @@
                     if (input[left] != input[right])
                     {
                         Swap(input, left, right);
-
+                        PermuteWithRepetition(input, left + 1, end);
                     }
                 }
+
+                //Take the element previous to current swapped
+                var swapElement = input[left];
+                for (int i = left; i <= end - 1; i++)
+                {
+                    //first Iteration takes
+                    //{ 5, 1, 4 --> if left == 0, then ->  1, 1, 4 -> 1, 4, 4}
+                    input[i] = input[i + 1];
+                }
+                //last set takes the "swapElement" and sets it to the last position
+                //{ 1, 4, 5}
+                input[end] = swapElement;
             }
         }
 
@@ -35,6 +46,11 @@
             var temp = arr[firstIndex];
             arr[firstIndex] = arr[secondIndex];
             arr[secondIndex] = temp;
+        }
+
+        private static void Print<T>(T[] input)
+        {
+            Console.WriteLine(string.Join(" ", input).Trim());
         }
     }
 }
