b9a41ac56312727e72a330e2742a8116f1e9cb94
sapphire119 <sap.phire@abv.bg>
2020-07-12 20:02:33 +0300

    Solve p04.Best Lecture's Schedule & p05.EgyptianFractions

---
+++ Algos/02.Combinations&GreedyAlgos/Exercises/04.GreedyAlgos/GreedyAlgorithms/p04.Best Lecture's Schedule/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace p04.Best_Lecture_s_Schedule
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Cryptography.X509Certificates;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var lectures = new List<Lecture>();
+            var inputLecturesCount = int.Parse(Console.ReadLine().Split(' ')[1]);
+            for (int i = 0; i < inputLecturesCount; i++)
+            {
+                var inputLecture = Console.ReadLine();
+                var tokens = inputLecture.Split(':');
+                var lectureName = tokens[0];
+
+                var timeTokens = tokens[1].Trim().Split('-').Select(x => x.Trim()).Select(int.Parse).ToArray();
+                var startTime = timeTokens[0];
+                var endTime = timeTokens[1];
+                
+                lectures.Add(new Lecture(lectureName, startTime, endTime));
+            }
+
+            var currentLecture = lectures.OrderBy(x => x.EndTime).FirstOrDefault();
+
+            var result = new List<Lecture> { currentLecture };
+            foreach (var lecture in lectures.OrderBy(x => x.EndTime))
+            {
+                if(currentLecture.EndTime <= lecture.StartTime)
+                {
+                    result.Add(lecture);
+                    currentLecture = lecture;
+                }
+            }
+
+            Console.WriteLine($"Lectures ({result.Count}):");
+            Console.WriteLine($"{string.Join(Environment.NewLine, result)}");
+        }
+    }
+
+    public class Lecture
+    {
+        public Lecture(string name, int startTime, int endTime)
+        {
+            this.Name = name;
+            this.StartTime = startTime;
+            this.EndTime = endTime;
+        }
+
+        public string Name { get; set; }
+        public int StartTime { get; set; }
+        public int EndTime { get; set; }
+
+        public override string ToString()
+        {
+            //1-7 -> Java
+            return $"{this.StartTime}-{this.EndTime} -> {this.Name}";
+        }
+    }
+}
---
+++ Algos/02.Combinations&GreedyAlgos/Exercises/04.GreedyAlgos/GreedyAlgorithms/p05.EgyptianFractions/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace p05.EgyptianFractions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var inputExpression = Console.ReadLine();
+            var tokens = inputExpression.Split('/');
+            var numerator = int.Parse(tokens[0]);
+            var denominator = int.Parse(tokens[1]);
+            if(numerator >= denominator)
+            {
+                Console.WriteLine("Error (fraction is equal to or greater than 1)");
+                return;
+            }
+            var result = new List<string>();
+            EgyptianFractions(numerator, denominator, result);
+            Console.WriteLine($"{inputExpression} = {string.Join(" + ", result)}");
+            //var expressionToSolve = numerator / denominator;
+
+            //if(expressionToSolve >= 1)
+            //{
+            //    Console.WriteLine("Error (fraction is equal to or greater than 1)");
+            //    return;
+            //}
+
+            //expressionToSolve = Math.Round(expressionToSolve, 25);
+
+            //var numbers = new List<string>();
+
+            //var result = 0M;
+            //var currentNumber = 2M;
+
+            //while (Math.Round(result, 25) != expressionToSolve)
+            //{
+            //    decimal temp = 1 / currentNumber;
+            //    decimal tempResult = Math.Round(temp + result, 25);
+            //    if (tempResult <= expressionToSolve)
+            //    {
+            //        numbers.Add($"1/{currentNumber}");
+            //        result += temp;
+            //    }
+                
+            //    currentNumber++;
+            //}
+
+            //Console.WriteLine($"{inputExpression} = {string.Join(" + ", numbers)}");
+        }
+
+        public static void EgyptianFractions(int numerator, int denominator, List<string> result)
+        {
+            // If either numerator or 
+            // denominator is 0 
+            if (denominator == 0 || numerator == 0)
+                return;
+
+            // If numerator divides denominator,  
+            // then simple division  makes 
+            // the fraction in 1/n form 
+            if (denominator % numerator == 0)
+            {
+                result.Add($"1/{denominator / numerator}");
+                return;
+            }
+
+            // If denominator divides numerator,  
+            // then the given number is not fraction 
+            if (numerator % denominator == 0)
+            {
+                Console.Write(numerator / denominator);
+                return;
+            }
+
+            // We reach here dr > nr and dr%nr 
+            // is non-zero. Find ceiling of 
+            // dr/nr and print it as first 
+            // fraction 
+            int n = denominator / numerator + 1;
+            result.Add($"1/{n}");
+            //Console.Write("1/" + n + " + ");
+
+            // Recur for remaining part 
+            EgyptianFractions(numerator * n - denominator, denominator * n, result);
+        }
+    }
+}
