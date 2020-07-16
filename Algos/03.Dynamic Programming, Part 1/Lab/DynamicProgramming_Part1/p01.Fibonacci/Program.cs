224d2fd48c60bee069b3e8394ee57193b6e26c13
sapphire119 <sap.phire@abv.bg>
2020-07-16 15:51:36 +0300

    Modified p01.Fibonacci in order to pass judge

--- Algos/03.Dynamic Programming, Part 1/Lab/DynamicProgramming_Part1/p01.Fibonacci/Program.cs
+++ Algos/03.Dynamic Programming, Part 1/Lab/DynamicProgramming_Part1/p01.Fibonacci/Program.cs
@@ -12,23 +12,29 @@
             var input = int.Parse(Console.ReadLine());
 
             var fibs = new int[2];
-            for (int i = 0; i <= input; i++)
+            for (int i = 0; i < input; i++)
             {
-                if (i == 0) fibs[i] = 0;
-                if (i == 1) fibs[i] = 1;
-                else if (i >= 2)
+                if (i <= 1) fibs[i] = 1;
+                else
                 {
                     if (i % 2 == 0) fibs[i % 2] = fibs[i % 2] + fibs[i % 2 + 1];
                     else fibs[i % 2] = fibs[i % 2] + fibs[i % 2 - 1];
                 }
+                //if (i == 0) fibs[i] = 0;
+                //if (i == 1) fibs[i] = 1;
+                //else if (i >= 2)
+                //{
+                //    if (i % 2 == 0) fibs[i % 2] = fibs[i % 2] + fibs[i % 2 + 1];
+                //    else fibs[i % 2] = fibs[i % 2] + fibs[i % 2 - 1];
+                //}
             }
-
-            Console.WriteLine($"Bottom up method -> Number is {fibs[input % 2]}");
+            Console.WriteLine($"{fibs[(input - 1) % 2]}");
+            //Console.WriteLine($"Bottom up method -> Number is {fibs[input % 2]}");
 
             //Fibonacci Top-down
-            var dpTable = new Dictionary<int, int>();
-            var number = Fibonacci(input, dpTable);
-            Console.WriteLine($"Top down method -> Number is {number}");
+            //var dpTable = new Dictionary<int, int>();
+            //var number = Fibonacci(input, dpTable);
+            //Console.WriteLine($"Top down method -> Number is {number}");
         }
 
         private static int Fibonacci(int n, Dictionary<int, int> dpTable)
