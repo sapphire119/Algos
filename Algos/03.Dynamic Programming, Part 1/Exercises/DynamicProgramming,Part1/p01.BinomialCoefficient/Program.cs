c3b03ca838988ec0c4535e3a9b5a164aebfe330d
sapphire119 <sap.phire@abv.bg>
2020-07-18 18:42:50 +0300

    Solve p01.BinomialCoefficientm and add project for p02.Longest Zig-Zag Subsequence

--- Algos/03.Dynamic Programming, Part 1/Exercises/DynamicProgramming,Part1/p01.BinomialCoefficient/Program.cs
+++ Algos/03.Dynamic Programming, Part 1/Exercises/DynamicProgramming,Part1/p01.BinomialCoefficient/Program.cs
@@ -1,12 +1,31 @@
 ﻿namespace p01.BinomialCoefficient
 {
     using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
 
     public class Program
     {
         public static void Main()
         {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+            var dpTable = new long[n + 1, k + 1];
+            
+            var result = BinomialCoefficient(n, k, dpTable);
+            Console.WriteLine(result);
+        }
 
+        private static long BinomialCoefficient(int n, int k, long[,] dpTable)
+        {
+            if (dpTable[n, k] > 0) return dpTable[n, k];
+            if (k == 0) return 1;
+            if (n == k) return 1;
+            var firstPart = BinomialCoefficient(n - 1, k - 1, dpTable);
+            var secondPart = BinomialCoefficient(n - 1, k, dpTable);
+            dpTable[n, k] = firstPart + secondPart;
+            var result = firstPart + secondPart;
+            return result;
         }
     }
 }
---
+++ Algos/03.Dynamic Programming, Part 1/Exercises/DynamicProgramming,Part1/p02.Longest Zig-Zag Subsequence/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace p02.Longest_Zig_Zag_Subsequence
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
