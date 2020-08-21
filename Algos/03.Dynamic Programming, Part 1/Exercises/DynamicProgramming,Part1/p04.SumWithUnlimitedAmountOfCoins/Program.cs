9c0986167576d95026b1bfc46c6f74fdfc583d05
Yavor Vasilev <yavor.v@sbtech.com>
2020-08-21 17:52:21 +0300

    Upload

--- Algos/03.Dynamic Programming, Part 1/Exercises/DynamicProgramming,Part1/p04.SumWithUnlimitedAmountOfCoins/Program.cs
+++ Algos/03.Dynamic Programming, Part 1/Exercises/DynamicProgramming,Part1/p04.SumWithUnlimitedAmountOfCoins/Program.cs
@@ -7,14 +7,34 @@
         public static void Main()
         {
             var inputArr = new int[] { 1, 2, 3, 4, 6 };
-            var sum = 6;
+            var sum = 8;
 
             var combinations = 0;
-            for (int i = 0; i < inputArr.Length; i++)
+
+
+            
+            if (sum % inputArr[0] == 0) combinations++;
+
+            for (int i = 1; i < inputArr.Length; i++)
             {
-                var currentNumber = inputArr[i];
-                combinations += FetchAllCombinationsWithCurrentNumb(currentNumber, i, inputArr, sum);
+                var currentNumb = inputArr[i];
+                
+                var tempSum = currentNumb;
+                for (int j = 0; j < i; j++)
+                {
+
+                    if (j == 0 && (sum - tempSum) % inputArr[j] == 0) { combinations++; j++; }
+                    if (tempSum + inputArr[j] <= sum && j != 0) { tempSum += inputArr[j]; j = -1; }
+                }
             }
+            
+
+            //combinations += FetchAllCombinationsWithCurrentNumb(currentNumber, i, inputArr, sum);
+        }
+
+        private static void Test()
+        {
+
         }
 
         private static int FetchAllCombinationsWithCurrentNumb(int currentNumber, int index, int[] inputArr, int sum)
