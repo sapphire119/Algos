a8f8731b491ba51abc58bbdbeabd69c5a9e83bd5
sapphire119 <sap.phire@abv.bg>
2020-08-22 15:55:08 +0300

    TEMP

--- Algos/03.Dynamic Programming, Part 1/Exercises/DynamicProgramming,Part1/p04.SumWithUnlimitedAmountOfCoins/Program.cs
+++ Algos/03.Dynamic Programming, Part 1/Exercises/DynamicProgramming,Part1/p04.SumWithUnlimitedAmountOfCoins/Program.cs
@@ -1,68 +1,43 @@
 ﻿namespace p04.SumWithUnlimitedAmountOfCoins
 {
     using System;
+    using System.Linq;
 
     public class Program
     {
         public static void Main()
         {
+            //var inputArr = new int[] { 1, 2, 5, 10, 20, 50, 100 };
             var inputArr = new int[] { 1, 2, 3, 4, 6 };
+            //var inputArr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            //var sum = int.Parse(Console.ReadLine());
             var sum = 8;
 
             var combinations = 0;
-
-
             
             if (sum % inputArr[0] == 0) combinations++;
 
             for (int i = 1; i < inputArr.Length; i++)
             {
                 var currentNumb = inputArr[i];
-                
-                var tempSum = currentNumb;
-                for (int j = 0; j < i; j++)
+                var accumulatedOriginalNumb = currentNumb;
+                var tempSum = accumulatedOriginalNumb;
+                for (int j = 0; j <= i; j++)
                 {
-
+                    //if(j != 0)
+                    //{
+                    //    accumulatedOriginalNumb += currentNumb;
+                    //    tempSum = accumulatedOriginalNumb;
+                    //    if (tempSum > sum) break;
+                    //    j = -1;
+                    //    continue;
+                    //}
+                    //if (tempMax < j) { tempMax = j; tempSum = currentNumb; }
                     if (j == 0 && (sum - tempSum) % inputArr[j] == 0) { combinations++; j++; }
                     if (tempSum + inputArr[j] <= sum && j != 0) { tempSum += inputArr[j]; j = -1; }
                 }
             }
-            
-
-            //combinations += FetchAllCombinationsWithCurrentNumb(currentNumber, i, inputArr, sum);
-        }
-
-        private static void Test()
-        {
-
-        }
-
-        private static int FetchAllCombinationsWithCurrentNumb(int currentNumber, int index, int[] inputArr, int sum)
-        {
-            //case when a number cannot divide
-            if (index == 0)
-                if (sum % currentNumber == 0) return 1;
-                else return 0;
-
-            var combinations = 0;
-            var tempSum = currentNumber;
-            while (tempSum <= sum)
-            {
-
-                //for (int i = 0; i < index; i++)
-                //{
-
-                //}
-                tempSum += currentNumber;
-            }
-
-            if (tempSum == sum) combinations++;
-            //var tempSum = currentNumber;
-            //while (sum - tempSum != 0)
-            //{
-
-            //}
-            return combinations;
+            Console.WriteLine(combinations);
         }
     }
 }
