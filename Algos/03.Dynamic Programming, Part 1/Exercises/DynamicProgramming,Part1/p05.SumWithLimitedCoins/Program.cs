d573d389ffc6c179ed2c89a94e6f0c6040795c75
Yavor Vasilev <yavor.v@sbtech.com>
2020-08-27 13:24:41 +0300

    Solve p05.SumWithLimitedCoins

--- Algos/03.Dynamic Programming, Part 1/Exercises/DynamicProgramming,Part1/p05.SumWithLimitedCoins/Program.cs
+++ Algos/03.Dynamic Programming, Part 1/Exercises/DynamicProgramming,Part1/p05.SumWithLimitedCoins/Program.cs
@@ -1,12 +1,32 @@
 ﻿namespace p05.SumWithLimitedCoins
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
 
     public class Program
     {
         public static void Main()
         {
+            var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var sum = int.Parse(Console.ReadLine());
+            var combinations = 0;
 
+            var list = new List<int>();
+            list.Add(0);
+            for (int i = 0; i < input.Length; i++)
+            {
+                var currentNum = input[i];
+                var oldList = new List<int>(list);
+                foreach (var numb in oldList)
+                {
+                    var temp = numb + currentNum;
+                    if (sum == temp)
+                    { combinations++; break; }
+                    list.Add(temp);
+                }
+            }
+            Console.WriteLine(combinations);
         }
     }
 }
