3294f6ee952f973f70b403feee0e00c96aa48b74
Yavor Vasilev <yavor.v@sbtech.com>
2020-10-04 22:15:31 +0300

    Upload Lab for Graphs

---
+++ Algos/05.Graphs/06. Graphs-and-Graph-Algorithms-Lab-Skeleton/Graphs/01. Connected-Components/GraphConnectedComponents.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GraphConnectedComponents
+{
+    static List<int>[] graph = new List<int>[]
+    {
+        new List<int>() { 3, 6 },
+        new List<int>() { 3, 4, 5, 6 },
+        new List<int>() { 8 },
+        new List<int>() { 0, 1, 5 },
+        new List<int>() { 1, 6 },
+        new List<int>() { 1, 3 },
+        new List<int>() { 0, 1, 4 },
+        new List<int>() { },
+        new List<int>() { 2 }
+    };
+
+    static List<int> result = new List<int>();
+
+    public static void Main()
+    {
+        graph = ReadGraph();
+        FindGraphConnectedComponents();
+    }
+
+    private static List<int>[] ReadGraph()
+    {
+        int n = int.Parse(Console.ReadLine());
+        var graph = new List<int>[n];
+        for (int i = 0; i < n; i++)
+        {
+            graph[i] = Console.ReadLine()
+                .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToList();
+        }
+        return graph;
+    }
+
+    private static void FindGraphConnectedComponents()
+    {
+        var visited = new bool[graph.Length];
+        for (int i = 0; i < graph.Length; i++)
+        {
+            if (!visited[i])
+            {
+                DepthFirstSearch(i, visited);
+                Console.WriteLine("Connected component: {0}", string.Join(" ", result));
+                result.Clear();
+            }
+        }
+    }
+    
+    private static void DepthFirstSearch(int node, bool[] visited)
+    {
+        if (!visited[node])
+        {
+            visited[node] = true;
+            var currentNodeChildren = graph[node];
+            foreach (var childNode in currentNodeChildren)
+            {
+                DepthFirstSearch(childNode, visited);
+            }
+            result.Add(node);
+            //Console.Write("{0} ", node);
+        }
+    }
+}
---
+++ Algos/05.Graphs/06. Graphs-and-Graph-Algorithms-Lab-Skeleton/Graphs/02. Topological-Sorting/TopologicalSorter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TopologicalSorter
+{
+    private Dictionary<string, List<string>> graph;
+    private Dictionary<string, int> predecesorCount;
+
+    public TopologicalSorter(Dictionary<string, List<string>> graph)
+    {
+        this.graph = graph;
+        this.predecesorCount = new Dictionary<string, int>();
+    }
+
+
+    public ICollection<string> TopSort()
+    {
+        GetPredecessorCount(this.graph);
+        var result = new List<string>();
+
+        while (true)
+        {
+            var nodeToRemove = predecesorCount.Keys.Where(node => predecesorCount[node] == 0).FirstOrDefault();
+            if (nodeToRemove == null) break;
+
+            var children = this.graph[nodeToRemove];
+            foreach (var child in children)
+            {
+                predecesorCount[child]--;
+            }
+
+            result.Add(nodeToRemove);
+            this.graph.Remove(nodeToRemove);
+            this.predecesorCount.Remove(nodeToRemove);
+        }
+
+        if (graph.Count > 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return result;
+    }
+    private void GetPredecessorCount(Dictionary<string, List<string>> graph)
+    {
+        foreach (var kvp in graph)
+        {
+            var node = kvp.Key;
+            var children = kvp.Value;
+
+            if (!predecesorCount.ContainsKey(node)) predecesorCount[node] = 0;
+            foreach (var child in children)
+            {
+                if (!predecesorCount.ContainsKey(child)) predecesorCount[child] = 0;
+                predecesorCount[child]++;
+            }
+        }
+    }
+}
